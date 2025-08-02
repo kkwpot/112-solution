a122da7ff78bde1fab3d08f1d7bed4fa666440e4
kkwpot <kkwforwork@gmail.com>
2025-08-03 01:16:38 +0700

    Create lab01.py

---
+++ midterm/lab01.py
@@ -0,0 +1,31 @@
+# Lab 01 Problem
+
+# 01: Temperature
+print((9/5*C)+32, C+273.15)
+
+# 02: Seconds
+
+seconds = s1+s2
+
+hours = seconds // 3600
+seconds = seconds - hours*3600
+
+minutes = seconds // 60
+seconds = seconds - minutes*60
+
+# 03: Addition
+
+print("Addition:", int(a)+int(b))
+print("Concatenation:", a+b)
+
+# 04: Score
+
+print("Total:", midterm + final)
+print("Average:", (midterm + final)/2)
+
+# 05: Grass
+
+print("Mowing cost is", (block_length*block_width-house_length*house_width)*MOW_RATE, "baht.")
+
+# 06: Star Pattern
+print(char*n)
